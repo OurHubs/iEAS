51d159e4571695eaa5bf86697a9a1a2ba552112a
Lance <ysclc1987@163.com>
2015-06-02 15:16:16 +0800

    增加Service，及模块页面

---
+++ 2.Codes/iEAS/iEAS.Infrastructure/Module/Service/IPortalService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iEAS.Repository;
+
+namespace iEAS.Module
+{
+    public interface IPortalService : IDomainService<PortalInfo, iEASRepository>
+    {
+
+    }
+}
---
+++ 2.Codes/iEAS/iEAS.Infrastructure/Module/Service/Imp/MenuService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iEAS.Module.Service.Imp
+{
+    public class MenuService : IdentityDomainService<Menu, iEAS.Repository.iEASRepository>,IMenuService
+    {
+    }
+}
