b6b5253e3b21412f2bc9992cf0bc1cfde8cb6f26
Lance <ysclc1987@163.com>
2015-06-26 15:42:47 +0800

    栏目编辑 列表页面

---
+++ 2.Codes/iEAS/iEAS.Infrastructure.Web/Pages/Module/ChannelList.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace iEAS.Infrastructure.Web.Pages.Module
+{
+    public partial class ChannelList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
