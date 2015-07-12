96fa5903a86be7572986ecb9e38383b5b2be22f1
snta2008 <277252477@qq.com>
2015-07-12 18:52:54 +0800

    完成部门树

--- 2.Codes/iEAS/iEAS.Infrastructure.Web/Pages/Orgnization/DepartmentTree.aspx
+++ 2.Codes/iEAS/iEAS.Infrastructure.Web/Pages/Orgnization/DepartmentTree.aspx
@@ -1,9 +1,8 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true"
+﻿<%@ Page Language="C#" AutoEventWireup="true" 
     MasterPageFile="~/Masters/Blank.Master"
-    CodeBehind="CompanyTree.aspx.cs" Inherits="iEAS.Infrastructure.Web.Pages.Orgnization.CompanyTree" %>
+    CodeBehind="DepartmentTree.aspx.cs" Inherits="iEAS.Infrastructure.Web.Pages.Orgnization.DepartmentTree" %>
 
-
-<asp:Content ID="Content1" runat="server" ContentPlaceHolderID="Header">
+<asp:Content ID="ctHeader" runat="server" ContentPlaceHolderID="Header">
     <link href="<%=Page.ResolveUrl("~/") %>assets/ztree/css/zTreeStyle/zTreeStyle.css" rel="stylesheet" />
     <script src="<%=Page.ResolveUrl("~/") %>assets/ztree/js/jquery.ztree.all-3.5.min.js" type="text/javascript"></script>
     <script type="text/javascript">
@@ -37,7 +36,7 @@
     </style>
 
 </asp:Content>
-<asp:Content ID="Content2" runat="server" ContentPlaceHolderID="Content">
+<asp:Content ID="ctContent" runat="server" ContentPlaceHolderID="Content">
     <div style="background: #fefefe; margin: 0 10px 0 0; height: 600px;">
         <ul id="ctTree" class="ztree"></ul>
     </div>
---
+++ 2.Codes/iEAS/iEAS.Infrastructure.Web/Pages/Orgnization/DepartmentTree.aspx.cs
@@ -0,0 +1,62 @@
+﻿using iEAS.Config;
+using iEAS.Orgnization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace iEAS.Infrastructure.Web.Pages.Orgnization
+{
+    public partial class DepartmentTree : System.Web.UI.Page
+    {
+
+        public ICompanyService CompanyService { get; set; }
+        public IDepartmentService DepartmentService { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string BuildTreeNodes()
+        {
+            var companies = CompanyService.Query(m => m.Status == 1, o => o.Asc(m => m.SN));
+            var departments = DepartmentService.Query(m => m.Company.Status==1 && m.Status == 1, o => o.Asc(m => m.SN));
+
+            StringBuilder sbTreeNodes = new StringBuilder();
+            sbTreeNodes.Append("[");
+            sbTreeNodes.AppendFormat("{{id:'{0}',pId:{1},name:'{2}',open:true,url:'{3}',target:'main'}},", Guid.Empty, "null", SiteConfig.Instance.Title, String.Empty);
+
+            foreach(var company in companies)
+            {
+                sbTreeNodes.AppendFormat("{{id:'{0}',pId:'{1}',name:'{2}',open:true,url:'{3}',target:'main'}},", company.ID, Guid.Empty, company.Name, GetUrl(company));
+            }
+
+            foreach (var department in departments)
+            {
+                if (department.ParentID == null)
+                {
+                    sbTreeNodes.AppendFormat("{{id:'{0}',pId:'{1}',name:'{2}',open:true,url:'{3}',target:'main'}},", department.ID,department.CompanyID, department.Name, GetUrl(department));
+                }
+                else
+                {
+                    sbTreeNodes.AppendFormat("{{id:'{0}',pId:'{1}',name:'{2}',open:true,url:'{3}',target:'main'}},", department.ID, department.ParentID, department.Name, GetUrl(department));
+                }
+            }
+            sbTreeNodes.Trim(',').Append(']');
+            return sbTreeNodes.ToString();
+        }
+
+        private string GetUrl(Company node)
+        {
+            return String.Empty;
+        }
+        private string GetUrl(Department node)
+        {
+            return node != null ? String.Format("EmployeeList.aspx?departmentId={0}", node.ID) : String.Empty;
+        }
+    }
+}
