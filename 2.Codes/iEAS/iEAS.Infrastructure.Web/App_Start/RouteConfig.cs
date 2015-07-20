39998c8ae1e47c3114744ae274ff750e6471823f
snta2008 <snta2008@126.com>
2015-07-20 16:04:46 +0800

    添加WebAPI

---
+++ 2.Codes/iEAS/iEAS.Infrastructure.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace iEAS.Infrastructure.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            RouteTable.Routes.MapPageRoute("Home", "", "~/Portal/TemplateEngine/Home.aspx");
+            RouteValueDictionary defChannel = new RouteValueDictionary();
+            defChannel.Add("PageIndex", 1);
+            RouteTable.Routes.MapPageRoute("ChannelList", "Channel/{ChannelID}_{PageIndex}", "~/Portal/TemplateEngine/Channel.aspx", false, defChannel);
+            RouteTable.Routes.MapPageRoute("Channel", "Channel/{ChannelID}", "~/Portal/TemplateEngine/Channel.aspx", false, defChannel);
+            RouteTable.Routes.MapPageRoute("ChannelDetail", "Detail/{ChannelID}_{RecordID}", "~/Portal/TemplateEngine/Detail.aspx");
+            RouteTable.Routes.MapPageRoute("Detail", "Detail/{RecordID}", "~/Portal/TemplateEngine/Detail.aspx");
+            RouteTable.Routes.MapPageRoute("ModelQuery", "ModelQuery/{Model}", "~/Model/ModelQuery.aspx");
+            RouteTable.Routes.MapPageRoute("ModelEdit", "ModelEdit/{Model}", "~/Model/ModelEdit.aspx");
+
+            RouteValueDictionary defaults = new RouteValueDictionary();
+            defaults.Add("PortalCode", "Default");
+
+            RouteTable.Routes.MapPageRoute("Portal", "Portal/{PortalCode}", "~/Portal.aspx");
+        }
+    }
+}
---
+++ 2.Codes/iEAS/iEAS.Infrastructure.WebAPI/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace iEAS.Infrastructure.WebAPI
+{
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> CheckUser()
+        {
+            return new List<string>
+            {
+                "ABC",
+                "DEF",
+                "GHI",
+                "AAA"
+            };
+        }
+    }
+}
